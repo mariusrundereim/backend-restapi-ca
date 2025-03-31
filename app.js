ad619b26b2283d317fd7b83a0da32f1977e22de8
Marius <marius.rundereim@gmail.com>
2025-03-31 12:28:54 +0200

    gitignore

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ db.sequelize.sync({ force: true });
 
 var app = express();
 
-// Routes
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -23,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Routes
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
